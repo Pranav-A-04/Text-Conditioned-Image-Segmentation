d4506c30c8ce46cf9b4d8dcaca197e2d78936195
Pranav-A-04 <pranavashok2@gmail.com>
2026-01-04 14:51:22 +0530

    fixing dice loss

--- utils/misc.py
+++ utils/misc.py
@@ -1,8 +1,11 @@
+import torch
+
 def dice_loss(predicted_mask, ground_truth_mask, smooth=1e-6):
-    predicted_mask = predicted_mask.view(-1)
-    ground_truth_mask = ground_truth_mask.view(-1)
+    probs = torch.sigmoid(predicted_mask)
+    predicted_mask = probs.view(probs.size(0), -1)
+    ground_truth_mask = ground_truth_mask.view(probs.size(0), -1)
     
-    intersection = (predicted_mask * ground_truth_mask).sum()
-    dice_score = (2. * intersection + smooth) / (predicted_mask.sum() + ground_truth_mask.sum() + smooth)
+    intersection = (predicted_mask * ground_truth_mask).sum(dim=1)
+    dice_score = (2. * intersection + smooth) / (predicted_mask.sum(dim=1) + ground_truth_mask.sum(dim=1) + smooth)
     
-    return 1 - dice_score
+    return 1 - dice_score.mean()
